de4b47921c2564fea88f8ace26c961dbd0c33c5d
PrachiKanodia08 <prachikanodia08@gmail.com>
2026-06-23 23:03:26 +0530

    Home titles and subtitles

--- src/components/HomePage/HomeBody.jsx
+++ src/components/HomePage/HomeBody.jsx
@@ -3,7 +3,21 @@ import HomeCards from './HomeCards'
 
 const HomeBody = () => {
   return (
-    <div className='bg-blue-50 h-screen'>
+    <div className='bg-blue-50 h-screen flex flex-col items-center gap-6 p-12'>
+        <div className='font-medium text-purple-700 bg-purple-100 rounded-full py-2 px-4'>
+          🌟 Free Student Community Platform
+        </div>
+        <div class="text-4xl font-bold flex flex-col items-center">
+          <p>
+            Share Knowledge,
+          </p>  
+          <p class="bg-gradient-to-r from-pink-600 via-orange-400 to-purple-600 bg-clip-text text-transparent">
+            Ace Your Exams! 🚀
+          </p> 
+        </div>
+        <div class="text-lg text-gray-600 flex items-center">
+          Join thousands of students sharing study notes and helping each other succeed. Upload your notes or download from our growing collection!
+        </div>
         <HomeCards />
     </div>
   )
--- src/components/HomePage/HomeCards.jsx
+++ src/components/HomePage/HomeCards.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 
 const HomeCards = () => {
+  const homeOptions = [
+    { 
+      "id":1,
+      "title": "Browse Notes",
+      "subtitle": "Access quality study materials from top students"
+    },
+    { 
+      "id":2,
+      "title": "Login",
+      "subtitle": "Access your notes"
+    }
+  ]
+
   return (
-    <div>HomeCards</div>
+    <div>
+      <div className='grid grid-cols-1 gap-6'>
+        {homeOptions.map((option) => (
+          <div key = {option.id} className='bg-white/80 text-center border border-purple-100 shadow-lg rounded-2xl p-4' >
+            <h2 className='font-bold'>{option.title}</h2>
+            <p className='font-medium text-gray-600'>{option.subtitle}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
